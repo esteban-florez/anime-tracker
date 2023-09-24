b2926e6453d258518f195d63f7a0781ee2da1fa9
esteban-florez <eflorez077@gmail.com>
2023-09-24 15:49:03 -0400

    added anime update route.ts

--- app/api/anime/[id]/route.ts
+++ app/api/anime/[id]/route.ts
@@ -1,11 +1,24 @@
 import prisma from "#/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-interface RouteParams {
-  params: { id: string }
+export async function PUT(request: NextRequest, { params: { id } }: Params) {
+  const data = await request.json()
+
+  const anime = await prisma.anime.update({
+    where: { id },
+    data: {
+      ...data,
+      year: Number(data.year),
+    }
+  })
+
+  return NextResponse.json({
+    updated: anime,
+    message: 'The anime was updated successfully',
+  })
 }
 
-export async function DELETE(_: NextRequest, { params: { id } }: RouteParams) {
+export async function DELETE(_: NextRequest, { params: { id } }: Params) {
   const anime = await prisma.anime.delete({
     where: { id }
   })
